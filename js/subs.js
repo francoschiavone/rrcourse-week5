10445c1797257fdc9bc4d97d0efef16f91611f6f
francoschiavone <francoaschiavone@gmail.com>
2020-11-02 12:57:04 -0300

    linked frontpageand subscription form

---
+++ js/subs.js
@@ -0,0 +1,10 @@
+//Subscribe form link button
+
+/*
+var subs = document.getElementsById("subs")
+subs.addEventListener('click', redirect);
+*/
+
+function redirect(){
+  window.location.href = "./subscription.html";
+}
